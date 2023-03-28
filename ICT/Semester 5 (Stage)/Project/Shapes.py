643873037ad1e100eb716904dcc9a9094447ec0f
Your New Username <a.almasri@student.fontys.nl>
2023-03-28 14:39:51 +0100

    nnn

---
+++ ICT/Semester 5 (Stage)/Project/Shapes.py
@@ -0,0 +1,6 @@
+from Geo import  GeometricPatterns
+
+
+
+Colors =["#3F12B6","#3F12B6","#3F12B6"]
+GeometricPatterns(Colors)
---
+++ ICT/Semester 5 (Stage)/Project/tesst.py
@@ -0,0 +1,4 @@
+from Geo import GeometricPatterns
+
+Colorss = ['#ff5733', '#ff5733', '#ff5733']
+zbb = GeometricPatterns(Colorss)
