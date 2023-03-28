64f31dd31d48583008e06418f5ab54257eb74436
Ammar Almasri <447874@student.fontys.nl>
2023-03-28 16:53:35 +0100

    Upload-project

---
+++ ICT/Semester 6/Individueel project/Prototype/Login_server/src/main/java/com/example/Login_server/Users_Controller.java
@@ -0,0 +1,18 @@
+package com.example.Login_server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Users_Controller {
+
+    @GetMapping("Users")
+    public String Getusers ()
+    {
+        return  "There are no users yet";
+    }
+
+
+
+
+}
