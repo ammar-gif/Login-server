7d465ae5e22ae939c064e2fa7f79639b07af5705
ammar-gif <73993769+ammar-gif@users.noreply.github.com>
2023-03-28 17:09:08 +0100

    Delete ICT/Semester 5 (Stage) directory

--- ICT/Semester 5 (Stage)/Project/Shapes.py
+++
@@ -1,6 +0,0 @@
-from Geo import  GeometricPatterns
-
-
-
-Colors =["#3F12B6","#3F12B6","#3F12B6"]
-GeometricPatterns(Colors)
--- ICT/Semester 5 (Stage)/Project/tesst.py
+++
@@ -1,4 +0,0 @@
-from Geo import GeometricPatterns
-
-Colorss = ['#ff5733', '#ff5733', '#ff5733']
-zbb = GeometricPatterns(Colorss)
