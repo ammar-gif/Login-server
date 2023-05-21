fc07a9441fc681e911254cc6637e8d4572d2cad4
Ammar Almasri <447874@student.fontys.nl>
2023-05-21 11:45:25 +0100

    test

--- Login_server/src/main/java/com/example/Login_server/UsersServerApplication.java
+++ Login_server/src/main/java/com/example/Login_server/UsersServerApplication.java
@@ -2,7 +2,7 @@ package com.example.Login_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class UsersServerApplication {
 
